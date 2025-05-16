37fa1e1c6aa495f5127d7a1ac6b97607fa35dba8
Moiz Siddiqui <moizsidd05@gmail.com>
2025-05-16 23:46:12 +0500

    added typeorm and pg dependency in the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      port: 5432,
+      type: 'postgres',
+      host: 'localhost',
+      synchronize: true,
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      database: 'task-management',
+    }),
+  ],
 })
 export class AppModule {}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { TasksService } from './tasks.service';
+import { TasksRepository } from './task.repository';
 import { TasksController } from './tasks.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TasksRepository])],
   providers: [TasksService],
   controllers: [TasksController],
 })
